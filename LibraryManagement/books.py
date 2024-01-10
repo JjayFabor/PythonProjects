409bc77d706b3b93ee658255e65189a51d641ea7
JjayFabor <faborjaylordvhan@gmail.com>
2024-01-10 20:43:29 +0800

    Added a feature to remove books

--- LibraryManagement/books.py
+++ LibraryManagement/books.py
@@ -1,8 +1,8 @@
-from Database.database import BookDatabase
+from Database.database import LibraryDatabase
 
 class Books:
     def __init__(self):
-        self.book_database = BookDatabase()
+        self.library_database = LibraryDatabase()
 
     def __str__(self):
         return '''\nPlease select a number:
@@ -21,7 +21,7 @@ class Books:
 
     def list_books(self):
     
-        with self.book_database.connect_to_database() as conn:
+        with self.library_database.connect_to_database() as conn:
             self.cursor = conn.cursor()
 
             self.cursor.execute('SELECT * FROM BookTable')
@@ -32,12 +32,12 @@ class Books:
                 print(f"Author: {book[1]}")
                 print(f"Published Date: {book[2]}\n")
         
-        self.book_database.close_connection()
+        self.library_database.close_connection()
 
     def add_books(self):
         
         try: 
-            with self.book_database.connect_to_database() as conn:
+            with self.library_database.connect_to_database() as conn:
 
                 isBookAdded = False
                 self.cursor = conn.cursor()
@@ -59,7 +59,20 @@ class Books:
             print("Error: " + str(e))
 
     def remove_books(self):
-        pass
+        
+        try:
+            with self.library_database.connect_to_database() as conn:
+                self.cursor = conn.cursor()
+
+                book_title_to_remove = input("Enter the title of the book you want to remove: ")
+
+                self.cursor.execute(f'DELETE FROM BookTable WHERE Title = "{book_title_to_remove}"')
+                print(f'Book "{book_title_to_remove}" removed successfully!')
+
+                self.library_database.close_connection()
+
+        except Exception as e:
+            print(f'Error: ' + str(e))
 
     def borrow_books(self):
         pass
